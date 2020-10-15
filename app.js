582204e700a1a20654dae5b8269ee9d2bbe426f2
Srikanth Gandikota <srikanthgandikota81@gmail.com>
2020-10-15 23:49:17 +0530

    new

---
+++ app.js
@@ -0,0 +1,10 @@
+(function(){
+'use strict';
+angular.module('LunchCheck',[])
+.controller('LunchCheckController', LunchCheckController);
+LunchCheckController.$inject['$scope', '$filter'];
+  function LunchCheckController($scope, $filter)
+  {
+    $scope.name="Srikanth";
+  }
+})();
