cbc317698c09a2b5b23ee8cd9cc742bb40322f2f
Srikanth Gandikota <srikanthgandikota81@gmail.com>
2020-10-16 23:00:45 +0530

    changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # module1-solution
+
+// browser-sync start --server --directory --files "**/*"
--- app.js
+++ app.js
@@ -1,10 +1,54 @@
-(function(){
-'use strict';
-angular.module('LunchCheck',[])
-.controller('LunchCheckController', LunchCheckController);
-LunchCheckController.$inject['$scope', '$filter'];
-  function LunchCheckController($scope, $filter)
-  {
-    $scope.name="Srikanth";
-  }
+(function () {
+	'use strict';
+
+	angular.module('LunchCheck',[]).controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject =['$scope'];
+
+	function LunchCheckController ($scope){
+		$scope.list = '';
+		$scope.message = '';
+		$scope.fontStyle = {};
+		$scope.boxStyle = {};
+
+		$scope.checkItems = function () {
+			if($scope.list !== '') {
+				var arr = $scope.list.split(',');
+				var count =0;
+				for(var i =0; i<arr.length; i++){
+					if(arr[i].trim() !=='')
+						count++;
+				}
+				if(count > 0){
+					if(count <=3){
+						$scope.message = 'Enjoy!';
+					}
+					else if(count > 3){
+						$scope.message = 'Too much!'
+					}
+					$scope.fontStyle = {
+						"color":"green"
+					};
+					$scope.boxStyle = {
+						"border-color":"green"
+					};
+				}
+				else {
+					enterItems();
+				}
+			}
+			else {
+				enterItems();
+			}
+		}
+		var enterItems = function() {
+			$scope.message = 'Please enter data first';
+				$scope.fontStyle = {
+					"color":"red"
+				};
+				$scope.boxStyle = {
+					"border-color":"red"
+				};
+		};
+	}
 })();
